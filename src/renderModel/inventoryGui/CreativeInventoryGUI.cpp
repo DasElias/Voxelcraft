88753687a65e4724aa817e9720fc4dceea5594b7
Elias Horner <elias.horner@student.htldornbirn.at>
2019-09-26 17:24:35 +0200

    use AspectRatioElement to scale img correctly

--- src/renderModel/inventoryGui/CreativeInventoryGUI.cpp
+++ src/renderModel/inventoryGui/CreativeInventoryGUI.cpp
@@ -1,18 +1,27 @@
+
 #include "CreativeInventoryGUI.h"
 #include <model\nodes\UnorganizedParentElement.h>
 #include <model\nodes\VBox.h>
+#include <model\nodes\AspectRatioElement.h>
+
 #include <model/nodeComponents/background/TexturedBackground.h>
 
 #include <model/nodeComponents/background/ColoredBackground.h>
 #include "../../model/utils/FileUtils.h"
+#include <model/positioning/CenterAllInParentWrapper.h>
+#include <model/positioning/RelativePositioningInParent.h>
+
 
 using namespace egui;
 
 namespace vc {
 	CreativeInventoryGUI::CreativeInventoryGUI() {
-		std::shared_ptr<VBox> parent(new VBox());
-
 		static std::shared_ptr<Image> img = Image::loadTexture(vc::getApplicationFolder().append("\\textures\\gui\\creativeInventory.png"));
+		float imgWidth = img->getWidth();
+		float imgHeight = img->getHeight();
+		float ar = imgWidth / imgHeight;
+		std::shared_ptr<AspectRatioElement> parent(new AspectRatioElement(ar));
+		
 		parent->setBackground(std::shared_ptr<Background>(
 			new TexturedBackground(
 				img
@@ -20,7 +29,8 @@ namespace vc {
 		));
 
 
-		parent->setPreferredDimension(0.7f, true, 0.8f, true);
+		parent->setMaxDimension(0.8f, true, 0.8f, true);
+		parent->setOwnPositioning(std::shared_ptr<Positioning>(new egui::CenterAllInParentWrapper(std::shared_ptr<Positioning>(new RelativePositioningInParent(0, 0)))));
 		inventoryGUI.setRootElement(parent);
 	}
 
